3471b8beca37b5d4f94df10c4533b6edd9a126eb
excitexcite <excitenew@gmail.com>
2021-09-13 02:21:48 +0300

    Implement RecyclerView

--- app/src/main/java/com/excite/affirmations/MainActivity.kt
+++ app/src/main/java/com/excite/affirmations/MainActivity.kt
@@ -3,13 +3,17 @@ package com.excite.affirmations
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.excite.affirmations.adapter.ItemAdapter
 import com.excite.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView: TextView = findViewById(R.id.textView)
-        textView.text = Datasource().loadAffirmations().size.toString()
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
     }
 }
